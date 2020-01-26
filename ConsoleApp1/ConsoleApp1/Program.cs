44996edb9847c022c62755e889068f7241cb9dcd
BogdanKorniyko <korniykob2000@gmail.com>
2020-01-26 23:42:26 +0300

    ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int counter = 1;
+while (counter <= 7)
+{
+    System.Console.WriteLine("Значение счетчика: " + counter);
+    counter++;
+}
+    System.Console.ReadLine();
+
+        }
+
+    }
+}
+
+
