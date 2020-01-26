69f9af7b92426befb29b6ac7b04b136a51d6d926
BogdanKorniyko <korniykob2000@gmail.com>
2020-01-26 23:56:46 +0300

    A-01

---
+++ A-01/A01/A01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01
+{
+    class Program
+    {
+        public int Sum(int a, int b) => a + b;
+        //{
+        //return a+b
+
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedite a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vvedite b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Program program = new Program();
+            program.Sum(a, b);
+            Console.WriteLine("Summa: " + program.Sum(a, b));
+            Console.ReadKey();
+
+
+        }
+    }
+}
